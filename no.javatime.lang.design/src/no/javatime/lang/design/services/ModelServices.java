e15d48597eda9ad0ced431544bba107489d0ee6d
Eirik Gronsund <eg@iserv.no>
2016-07-04 15:02:21 +0200

    Returning edges in ModelServices

--- no.javatime.lang.design/src/no/javatime/lang/design/services/ModelServices.java
+++ no.javatime.lang.design/src/no/javatime/lang/design/services/ModelServices.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.XtextResource;
 
 import no.javatime.lang.model.Element;
 import no.javatime.lang.model.Field;
@@ -19,9 +30,42 @@ import no.javatime.lang.model.PackageDeclaration;
 public class ModelServices {
     
 	public EObject openTextEditor(EObject any) {
-		System.out.println("Double click invoking openTextEditor: "  + any);
+		if (any != null && any.eResource() instanceof XtextResource
+				&& any.eResource().getURI() != null) {
+
+			String fileURI = any.eResource().getURI().toPlatformString(true);
+			IFile workspaceFile = ResourcesPlugin.getWorkspace().getRoot()
+					.getFile(new Path(fileURI));
+			if (workspaceFile != null) {
+				IWorkbenchPage page = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage();
+				try {
+					IEditorPart openEditor = IDE.openEditor(page,
+							workspaceFile,
+							"no.javatime.lang.Model",
+							true);
+					if (openEditor instanceof AbstractTextEditor) {
+						ICompositeNode node = NodeModelUtils
+								.findActualNodeFor(any);
+						if (node != null) {
+							int offset = node.getOffset();
+							int length = node.getTotalEndOffset() - offset;
+							((AbstractTextEditor) openEditor).selectAndReveal(
+									offset, length);
+							System.out
+									.println("ModelServices.openTextEditor()");
+						}
+					}
+					// editorInput.
+				} catch (PartInitException e) {
+					// Put your exception handler here if you wish to.
+				}
+			}
+		}
+		System.out.println(any);
 		return any;
 	}
+
 	static int st = 0;
 
 	/**
